f4647133ed26d5d43d039029b8c563333164cdc0
Alexandre Baiao <albaiao@microsoft.com>
2019-10-24 10:34:25 -0300

    New stuff

--- .bash_aliases
+++ .bash_aliases
@@ -1,3 +1,4 @@
 
 alias l='ls -lhat'
 alias tmux='TERM=xterm-256color tmux'
+alias copy="xclip -sel clip"
--- .setup_commands_apt.sh
+++ .setup_commands_apt.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-apt install vim tmux docker python3 python3-pip
+apt install vim tmux docker python3 python3-pip xclip
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .* ~/
 cp plugins.vim ~/.vim/plugins.vim
+cd ~/repo
+git clone https://github.com/b-ryan/powerline-shell.git
 
 ./add_aliases.sh
